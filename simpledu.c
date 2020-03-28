a1b86e56a7636f848943f0da8421c72b2022122b
Ricardo Amaral Nunes <ricardo.nunes.1892@gmail.com>
2020-03-28 16:52:47 -0100

    Skeleton DONEgit add .gitignore simpledu.c!

--- simpledu.c
+++ simpledu.c
@@ -25,74 +25,98 @@ int main(int argc, char * argv[], char * envp[]){
         exit(1);
     }
 
-
-
-    
     //printf("Starting with values: %s %s %s\n", argv[0],argv[1], argv[2]);
     
     struct dirent *dir_entry;           //directory entry currently reading
     struct stat filestat;               //info about the file currently reading
 
     //pid_t pids[MAX_PIDS];             //array of pids of child processes
-     //it_pid=0,
+    //it_pid=0,
     
     int status;
 
-    DIR * home;                     //directory curently opening
+    DIR * home;                         //directory curently opening
 
+    char path[200];                     //current path
+    getcwd(path,200);
+
+    sprintf(path, "%s/%s", path, argv[2]);
+    
+    //printf("Path scanning: %s\n",path);
 
     //Opening directory
-    if((home = opendir(argv[2])) == NULL){
+    if((home = opendir(path)) == NULL){
         //perror(argv[1]);
         printf("Error opening dir\n");
         exit(2);
     }
 
     //Goes to the pretended directory
-    chdir(argv[2]);                  
-    
-    char buf[200];
-    printf("Path: %s\n", getcwd(buf, 200));
-    
+    //chdir(argv[2]);                  
+
     //Filling pid array
     //fillpids(pids, MAX_PIDS);
 
     while((dir_entry = readdir(home))!=NULL){
 
-        //LOG
-        //printf("Processing %s file in process %d\n", dir_entry->d_name, getpid());
+        char *filename = dir_entry->d_name;
 
-        //Reads file
-        stat(dir_entry->d_name, &filestat);
-
-
-        if(strcmp(dir_entry->d_name, ".")==0 || strcmp(dir_entry->d_name, "..") ==0){
+        if(strcmp(filename, ".")==0 || strcmp(filename, "..") ==0){
             continue;
         }
-        
 
-        //Verifies if is directory    
-        if(S_ISDIR(filestat.st_mode)){
+        //LOG
+        //printf("Processing %s file in process %d\n", filename, getpid());
+
+        char filepath[300];
+        if(sprintf(filepath, "%s/%s", argv[2], filename) < 0){
+            printf("Error in sprintf\n");
+            exit(5);
+        }
+
+        //printf("filepath: %s\n",filepath);
+
+        //Reads file
+        if(stat(filepath, &filestat)!=0){
+            printf("Error in stat\n");
+            exit(3);
+        }
+        
+        if(S_ISREG(filestat.st_mode)){
+
+            printf("%ld\t%s\n", filestat.st_size/1024,filepath);
+
+        }else if(S_ISDIR(filestat.st_mode)){          //Verifies if is directory  
             
             pid_t pid = fork();
+        
 
             if(pid == 0){        //Child process
                 
-                char *new_arg[3];
+                char * new_arg[3];
+                new_arg[2] = malloc(100);
                 
                 //copy_values(new_argv,argv,argc);
-
-                sprintf(new_arg[2], "%s", dir_entry->d_name);
                 
+                if(sprintf(new_arg[2], "%s/%s", argv[2] ,filename) < 0){
+                    printf("sprintf\n");
+                    exit(4);
+                }
+
+                //write(STDOUT_FILENO,"after sprintf\n",14);
+                
+                /*
                 if(strlen(argv[0]) == 10){
                     new_arg[0] = "../simpledu";
                 }else {
                     sprintf(new_arg[0], "../%s", argv[0]);
                 }
+                */
 
                 new_arg[1] = "-l";
+                new_arg[0]= argv[0];
 
-                //printf("Going to %s using prog %s with flag %s\n", new_arg[2], new_arg[0], new_arg[1]);    
+                //printf("Going to %s using prog %s with flag %s in process %d\n", new_arg[2], new_arg[0], new_arg[1], getpid());    
 
                 execvp(new_arg[0], new_arg);
                 printf("Error in executing recursive simpledu to %s\n", new_arg[2]);
@@ -104,10 +128,12 @@ int main(int argc, char * argv[], char * envp[]){
                     exit(5);
                 }
             }*/
-           
+            int ret;
+            while ((ret = wait(&status)) > 0); 
+            printf("%ld\t%s\n", filestat.st_size/1024,filename);
         }
        
-        printf("%ld\t%s\n", filestat.st_size/1024,dir_entry->d_name);
+       
 
         /*
         int ret;
