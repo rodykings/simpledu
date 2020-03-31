da85640d61bb0902857a413b6380faa412c3b1c7
Ricardo Amaral Nunes <ricardo.nunes.1892@gmail.com>
2020-03-31 21:45:24 +0000

    Struct Flags and method, help functionallity added!

--- simpledu.c
+++ simpledu.c
@@ -9,8 +9,15 @@
 #include <wait.h>
 #include <string.h>
 #include <fcntl.h>
+#include <stdbool.h>
 
 #define MAX_PIDS 20
+
+struct flags
+{
+    bool all, bytes, block_size, deref, sep_dirs, max_depth, clinks;
+    int nbytes, depth_level;
+};
 
 void copy_values(char *dest[], char *copy[], int n);
 
@@ -18,13 +25,28 @@ void fillpids(pid_t *a, int n);
 
 int putpid(pid_t *a, int n, pid_t pid);
 
-//void logIt(int fdLog, clock_t instant, pid_t pid, char * action, char * info);
-
 void transformToString(char *result, char *array[], int n);
 
 void log_create(char *logFileName, char *argv[], int argc);
 
 void log_exit(char * logFileName, int exit_status);
+
+void print_help(void);
+
+int fill_flags(struct flags *scpflags, char * argv[], int argc);
+
+//Function that helped the debug
+void print_flags(struct flags f);
+
+
+/*  Definição da struct que contém todas as flags
+    A presença das flags está definida com as variáveis bool
+    Algumas flags vêm com números associados, justificando-se assim as variaveis de tipo int
+*/
+
+
+
+
 
 int main(int argc, char * argv[], char * envp[]){
    
@@ -34,14 +56,28 @@ int main(int argc, char * argv[], char * envp[]){
     //Writes in LogFile
     log_create(logFile, argv, argc);
 
+    struct flags spcFlags;              //The flags on the argument line
+
+
+
+    if(strcmp(argv[1], "help") == 0 && argc == 2){
+        print_help();
+        log_exit(logFile, 0);
+        exit(0);
+    }
+
     //Error conditions
-    if(argc != 3){
-        printf("Usage: %s -l <dirname>\n", argv[0]);
+    if(argc > 9 || argc == 1){
+        printf("Usage: %s -l <flags> <dirname>\nFor more information about the flags, run %s help\n", argv[0], argv[0]);
         log_exit(logFile, 1);
         exit(1);
     }
-    
-    
+
+    if( fill_flags(&spcFlags,argv,argc)){
+        printf("Someting went wrong when setting up the flags!\n");
+        log_exit(logFile, 6);
+        exit(6);
+    }   
     
 
     //printf("Starting with values: %s %s %s\n", argv[0],argv[1], argv[2]);
@@ -169,7 +205,7 @@ int main(int argc, char * argv[], char * envp[]){
         /*
         int ret;
         while((ret = waitpid(-1, &status, WNOHANG) != 0) && ret != -1);
-*/
+        */
         //printf("passei o while ma friends!\n");
         
         //Verifica se algum processo filho acabou
@@ -185,7 +221,6 @@ int main(int argc, char * argv[], char * envp[]){
         }*/
     }
     
-    //printf("hello\n");
     //Waiting for all childs to end (needs to change!)
     //while(wait(&status)!=-1);
     int ret;
@@ -270,4 +305,88 @@ void log_exit(char * logFileName, int exit_status){
     write(fdLog, buff, strlen(buff));
     close(fdLog);
     
+}
+
+
+void print_help(void){
+    printf("-a , --all \t\t- a informação exibida diz respeito também a ficheiros\n");
+    printf("-b , --bytes \t\t- apresenta o número real de bytes de dados (ficheiros) ou alocados (diretórios)\n");
+    printf("-B , --block-size=SIZE \t- define o tamanho (bytes) do bloco para efeitos de representação\n");
+    printf("-l , --count-links \t- contabiliza múltiplas vezes o mesmo ficheiro\n");
+    printf("-L , --dereference \t- segue links simbolicos \n");
+    printf(" -S , --separate-dirs \t- a informação exibida não inclui o tamanho dos subdiretorios\n");
+    printf("--max-depth=N \t\t- limita a informação exibida a N (0,1, ...) níveis de profundidade de diretorios\n ");
+}
+
+int fill_flags(struct flags *scpflags, char * argv[], int argc){
+
+    int i=1;
+    scpflags->all=false; scpflags->block_size=false; scpflags->bytes=false; scpflags->clinks = false;
+    scpflags->depth_level=false; scpflags->deref =false; scpflags->max_depth = false; scpflags->sep_dirs = false;
+
+    while(argv[i+1] != NULL){
+        
+
+        if(!scpflags->all && (strcmp(argv[i], "-a") == 0 || strcmp(argv[i], "--all")== 0) ){
+            scpflags->all = true;
+
+        }else if(!scpflags->bytes && (strcmp(argv[i], "-b") == 0 || strcmp(argv[i], "--bytes")== 0) ){
+            scpflags->bytes=true;
+        
+        }else if(!scpflags->block_size && (strncmp(argv[i], "-B",2) == 0 || strncmp(argv[i], "--block-size=", 13)== 0) ){
+            scpflags->block_size = true;
+
+            char temp[50];
+            strcpy(temp, argv[i]);
+            strtok(temp, "=");
+            int itemp;
+
+            if(sscanf(strtok(NULL, " "), "%d", &itemp) == EOF){
+                printf("Error on sscnaf\n");
+                return -1;
+            }
+
+            scpflags->nbytes = itemp;
+
+        }else if(!scpflags->deref && (strcmp(argv[i], "-L") == 0 || strcmp(argv[i], "--dereference") == 0)){
+            scpflags->deref = true;
+        }else if(!scpflags->sep_dirs && (strcmp(argv[i], "-S") == 0 || strcmp(argv[i], "--separate-dirs") == 0)){
+            scpflags->sep_dirs = true;
+        
+        }else if(!scpflags->max_depth && strncmp(argv[i], "--max-depth=", 12    ) == 0){
+            scpflags->max_depth = true;
+            
+            char temp[50];
+            strcpy(temp, argv[i]);
+            strtok(temp, "=");
+            int itemp;
+
+            if(sscanf(strtok(NULL, " "), "%d", &itemp) == EOF) {
+                printf("Error on sscnaf\n");
+                return -1;
+            }
+
+            scpflags->depth_level = itemp;
+        }else if(!scpflags->clinks && (strcmp(argv[i], "-l") == 0 || strcmp(argv[i], "--count-links") == 0) ){
+            scpflags->clinks = true;
+        
+        }else{
+            printf("Other flag detected\n");
+            return -1;
+        }
+        i++;
+    }
+
+    return 0;
+}
+
+
+void print_flags(struct flags f){
+    printf("All: %d\n", f.all);
+    printf("Bytes: %d\n", f.bytes);
+    printf("Blocks: %d %d\n", f.block_size,f.nbytes);
+    printf("Link: %d\n", f.clinks);
+    printf("Deref: %d\n", f.deref);
+    printf("Separate: %d\n", f.sep_dirs);
+    printf("Max Depth: %d %d\n", f.max_depth,f.depth_level);
 }
